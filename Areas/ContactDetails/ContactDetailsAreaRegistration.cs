b0267e061318aad4bd1802843f5c726346e8ae88
Qinisela Elvis Molefe <molefeq@gmail.com>
2013-05-28 11:57:18 +0200

    Adding an area
    Adding a area for the assignment

---
+++ Areas/ContactDetails/ContactDetailsAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication1.Areas.ContactDetails
+{
+  public class ContactDetailsAreaRegistration : AreaRegistration
+  {
+    public override string AreaName
+    {
+      get
+      {
+        return "ContactDetails";
+      }
+    }
+
+    public override void RegisterArea(AreaRegistrationContext context)
+    {
+      context.MapRoute(
+          "ContactDetails_default",
+          "ContactDetails/{controller}/{action}/{id}",
+          new { action = "Index", id = UrlParameter.Optional }
+      );
+
+      context.MapRoute(
+          "Management_default",
+          "ContactDetails/{controller}/{action}/{id}",
+          new { action = "Index", controller="Management", id = UrlParameter.Optional }
+      );
+    }
+  }
+}
